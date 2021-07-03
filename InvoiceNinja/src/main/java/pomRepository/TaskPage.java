d6a1dd2f828d38685ced419d6113ea837edec4d8
nayanrajanna <nayanrajanna@nayan>
2021-07-03 13:40:35 +0530

    pom taskpage updated

--- InvoiceNinja/src/main/java/pomRepository/TaskPage.java
+++ InvoiceNinja/src/main/java/pomRepository/TaskPage.java
@@ -24,6 +24,66 @@ public class TaskPage extends BasePage {
 	
 	@FindBy(id="search")
 	private WebElement searchBar;
+	@FindBy(id="search")
+	private WebElement searchtextFiled;
+	@FindBy(xpath="//a[contains(text(),'Tasks')]")
+	private WebElement taskslink;
+	@FindBy(linkText="New Task")
+	private WebElement Newtasklink;
+	@FindBy(id="client")
+	private WebElement clienttextfield;
+	@FindBy(xpath=("//span[@data-dropdown='dropdown']"))
+	private WebElement clientdropdown;
+	@FindBy(id="project_name")
+	private WebElement projectname;
+	@FindBy(id="description")
+	private WebElement description;
+	@FindBy(xpath="//label[contains(text(),'Timer')]")
+	private WebElement timerradiobutton;
+	@FindBy(id="start-button")
+	private WebElement startbutton;
+	@FindBy(xpath="//label[contains(text(),'Manual')]")
+	private WebElement manualradiobutton;
+	@FindBy(xpath="//input[@placeholder='Start Time']")
+	private WebElement starttime;
+	public WebElement getStarttime() {
+		return starttime;
+	}
+	public WebElement getEndtime() {
+		return endtime;
+	}
+	@FindBy(xpath="//input[@placeholder='End Time']")
+	private WebElement endtime;
+	public WebElement getTimer() {
+		return timerradiobutton;
+	}
+	public WebElement getManualradiobutton() {
+		return manualradiobutton;
+	}
+	public WebElement getStartbutton() {
+		return startbutton;
+	}
+	public WebElement getSearchtextFiled() {
+		return searchtextFiled;
+	}
+	public WebElement getTaskslink() {
+		return taskslink;
+	}
+	public WebElement getNewtasklink() {
+		return Newtasklink;
+	}
+	public WebElement getClienttextfield() {
+		return clienttextfield;
+	}
+	public WebElement getClientdropdown() {
+		return clientdropdown;
+	}
+	public WebElement getProjectname() {
+		return projectname;
+	}
+	public WebElement getDescription() {
+		return description;
+	}
 	
 	public WebElement getSearchBar() {
 		return searchBar;
