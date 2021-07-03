d16439871ea872eb28b02cb0c0bb6510a57589c6
nayanrajanna <nayanrajanna@nayan>
2021-07-04 00:34:50 +0530

    commit

--- InvoiceNinja/src/main/java/pomRepository/TaskPage.java
+++ InvoiceNinja/src/main/java/pomRepository/TaskPage.java
@@ -31,6 +31,78 @@ public class TaskPage extends BasePage {
 	@FindBy(xpath="//div[text()='Done']/../../..//a[contains(text(),'New Task...')]")
 	private WebElement DropToDone; 
 	
+
+	//@FindBy(id="search")
+	//private WebElement searchtextFiled;
+	@FindBy(xpath="//a[contains(text(),'Tasks')]")
+	private WebElement taskslink;
+	
+	@FindBy(linkText="New Task")
+	private WebElement Newtasklink;
+	
+	@FindBy(id="client")
+	private WebElement clienttextfield;
+	
+	@FindBy(xpath=("//span[@data-dropdown='dropdown']"))
+	private WebElement clientdropdown;
+	
+	public WebElement getTaskslink() {
+		return taskslink;
+	}
+
+	public WebElement getNewtasklink() {
+		return Newtasklink;
+	}
+
+	public WebElement getClienttextfield() {
+		return clienttextfield;
+	}
+
+	public WebElement getClientdropdown() {
+		return clientdropdown;
+	}
+
+	public WebElement getProjectname() {
+		return projectname;
+	}
+
+	public WebElement getDescription() {
+		return description;
+	}
+
+	public WebElement getTimerradiobutton() {
+		return timerradiobutton;
+	}
+
+	public WebElement getStartbutton() {
+		return startbutton;
+	}
+
+	public WebElement getManualradiobutton() {
+		return manualradiobutton;
+	}
+
+	public WebElement getStarttime() {
+		return starttime;
+	}
+	@FindBy(id="project_name")
+	private WebElement projectname;
+	
+	@FindBy(id="description")
+	private WebElement description;
+	
+	@FindBy(xpath="//label[contains(text(),'Timer')]")
+	private WebElement timerradiobutton;
+	
+	@FindBy(id="start-button")
+	private WebElement startbutton;
+	
+	@FindBy(xpath="//label[contains(text(),'Manual')]")
+	private WebElement manualradiobutton;
+	
+	@FindBy(xpath="//input[@placeholder='Start Time']")
+	private WebElement starttime;
+	
 	public WebElement getDragfromBacklog() {
 		return DragfromBacklog;
 	}
